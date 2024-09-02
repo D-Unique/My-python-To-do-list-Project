0dd62fa50c4467e2da82e5f3640ed80fdb4d7cd3
Unigwe <emmunigwe@gmail.com>
2024-09-02 13:21:56 +0100

    update

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, redirect, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired, email, equal_to
+#from flask_sqlalchemy import SQLAlchemy
 #from datetime import datetime 
 
 # create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "My supersecret key"
 app.url_map.strict_slashes = False
 users = ( 'Maryann', 'Anthony', 'Darren', 'Emma')
+
+# create a class form
+class SignUpForm(FlaskForm):
+    fname = StringField("First Name", validators=[DataRequired()])
+    lname = StringField("Last Name", validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    passwd = PasswordField('password', validators=[DataRequired()])
+    compasswd = PasswordField('comfirm password', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+
 #add database
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
@@ -22,14 +37,51 @@ users = ( 'Maryann', 'Anthony', 'Darren', 'Emma')
         return '<Name %r>' %self.name"""
 
 # create a route decorator
-@app.route('/sign in')
-def signin():
-    return 'PLEASE SIGN IN'
 
+#Sign in page
+@app.route('/sign in', methods=['GET', 'POST'])
+def signin():
+    fname = None
+    lname = None
+    email = None
+    passwd = None
+    compasswd = None
+    form = SignUpForm()
+    
+    
+    #validate Form
+    if form.validate_on_submit():
+        #fname
+        fname = form.fname.data
+        form.fname.data = ''
+        #lname
+        lname = form.lname.data
+        form.lname.data = ''
+        #email
+        email = form.email.data
+        form.email.data = ''
+        #passwd
+        passwd= form.passwd.data
+        form.passwd.data = ''
+        #compasswd
+        compasswd= form.compasswd.data
+        form.compasswd.data = ''
+        flash("Account Created!")
+
+    return render_template('signup.html', fname = fname, lname = lname, email = email, passwd = passwd, compasswd = compasswd, form = form)
+
+#landing page
 @app.route('/')
 def home():
-    return 'HOME PAGE'
+    return render_template('landing_page.html')
 
+#about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+#Index page
 @app.route('/<myurs>')
 def index(myurs):
     if myurs in users:
@@ -37,6 +89,7 @@ def index(myurs):
     else:
         return redirect(url_for('signin'))
 
+#add task page
 @app.route('/task/<Turs>')
 def task(Turs):
     return render_template('add_task.html', urs=Turs)
@@ -51,6 +104,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server(e):
     return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
